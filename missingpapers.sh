888484de8afc002371b3506c7ae0e5a6413d0d26
Andrew Gray <github@generalist.org.uk>
2016-06-09 13:54:07 +0100

    cmt

--- missingpapers.sh
+++ missingpapers.sh
@@ -12,6 +12,7 @@
 
 # all dates currently 2015-16 only, may make this adjustable for final version
 # commented out to avoid spamming the same request at repos
+# be careful - a full year's data might be >100MB for a large repository!
 
 # curl "http://nora.nerc.ac.uk/cgi/search/archive/advanced/export_nora_JSON.js?screen=Search&dataset=archive&_action_export=1&output=JSON&exp=0%7C1%7C-date%2Fcreators_name%2Ftitle%7Carchive%7C-%7Cdate%3Adate%3AALL%3AEQ%3A2015-2016%7Ctype%3Atype%3AANY%3AEQ%3Aarticle%7C-%7Ceprint_status%3Aeprint_status%3AANY%3AEQ%3Aarchive%7Cmetadata_visibility%3Ametadata_visibility%3AANY%3AEQ%3Ashow&n=" > working/json-nora
 
